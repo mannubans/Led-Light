351f0aac18963334fee22618add8846c99b22406
Mannu Bansal <mannubans@gmail.com>
2022-02-22 22:01:59 +0530

    added

--- js/custom.js
+++ js/custom.js
@@ -46,7 +46,7 @@
   $(window).scroll(function () {
     var window_top = $(window).scrollTop() + 1;
     if (window_top > 50) {
-      $('.main_menu_iner').addClass('menu_fixed animated fadeInDown');
+      $('.main_menu_iner').addClass('menu_fixed animated fadeInDown');      
     } else {
       $('.main_menu_iner').removeClass('menu_fixed animated fadeInDown');
     }
